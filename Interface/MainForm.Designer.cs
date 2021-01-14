7922f62060efcdb7dcec52cde4e1a7dc45f774eb
CodingRacoonn <piotra.duchnowski@gmail.com>
2021-01-14 14:24:46 +0100

    Added Windows Form Project

---
+++ Interface/MainForm.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace Interface
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.OpenFile = new System.Windows.Forms.Button();
+            this.AdjustSubs = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.saveButon = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(108, 16);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(718, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // OpenFile
+            // 
+            this.OpenFile.Location = new System.Drawing.Point(13, 13);
+            this.OpenFile.Name = "OpenFile";
+            this.OpenFile.Size = new System.Drawing.Size(75, 23);
+            this.OpenFile.TabIndex = 2;
+            this.OpenFile.Text = "Wczytaj plik";
+            this.OpenFile.UseVisualStyleBackColor = true;
+            this.OpenFile.Click += new System.EventHandler(this.OpenFile_Click);
+            // 
+            // AdjustSubs
+            // 
+            this.AdjustSubs.Location = new System.Drawing.Point(12, 54);
+            this.AdjustSubs.Name = "AdjustSubs";
+            this.AdjustSubs.Size = new System.Drawing.Size(118, 23);
+            this.AdjustSubs.TabIndex = 3;
+            this.AdjustSubs.Text = "Aktualizacja czasu";
+            this.AdjustSubs.UseVisualStyleBackColor = true;
+            this.AdjustSubs.Click += new System.EventHandler(this.AdjustSubs_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(146, 56);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(72, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(146, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(223, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Czas w milisekundach, przykłady (1500, -700)";
+            // 
+            // saveButon
+            // 
+            this.saveButon.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.saveButon.Location = new System.Drawing.Point(13, 494);
+            this.saveButon.Name = "saveButon";
+            this.saveButon.Size = new System.Drawing.Size(199, 37);
+            this.saveButon.TabIndex = 7;
+            this.saveButon.Text = "Zapisz";
+            this.saveButon.UseVisualStyleBackColor = true;
+            this.saveButon.Click += new System.EventHandler(this.saveButon_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox4.Location = new System.Drawing.Point(235, 509);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(591, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(238, 495);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Nazwa pliku do zapisu";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 83);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(814, 405);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(838, 543);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.saveButon);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.AdjustSubs);
+            this.Controls.Add(this.OpenFile);
+            this.Controls.Add(this.textBox2);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button OpenFile;
+        private System.Windows.Forms.Button AdjustSubs;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button saveButon;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox textBox1;
+    }
+}
---
+++ Interface/MainForm.cs
@@ -0,0 +1,408 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Interface
+{
+    public partial class MainForm : Form
+    {
+        //List of sting used in adjusting timers
+        List<string> lines;
+        List<string> newLines;
+        //File name and parent path
+        string fileName;
+        string parentPath;
+        //Number of updated properly and failed rows with time stamps
+        int counter;
+        int skipped;
+
+        //Form load
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        //On button click functions
+        private void OpenFile_Click(object sender, EventArgs e)
+        {
+            LoadSubtitles();
+        }
+
+        private void AdjustSubs_Click(object sender, EventArgs e)
+        {
+            if (lines == null || lines?.Count == 0)
+            {
+                MessageBox.Show("Wybierz plik", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                AdjustSubtitles();
+            }
+        }
+
+        private void saveButon_Click(object sender, EventArgs e)
+        {
+            if (textBox4.Text.Length == 0)
+            {
+                MessageBox.Show("Wpisz nazwę pliku", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                SaveToFile();
+            }
+        }
+
+
+        //Function which loads .srt file
+        private void LoadSubtitles()
+        {
+            lines = new List<string>();
+
+            string filePath = "";
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                //file dialog settings
+                openFileDialog.Filter = "srt files (*.srt)|*.srt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+                    //Get parent directory 
+                    GetParentPath(filePath);
+                    //Name of adjusted file 
+                    textBox2.Text = fileName;
+                    //Putting string lines into list
+                    lines = File.ReadAllLines(filePath).ToList();
+                    //Setting up textBox with loaded file
+                    UpdateTextBox(lines);
+                }
+            }
+        }
+
+        private void AdjustSubtitles()
+        {
+
+            newLines = new List<string>();
+            //counters setup
+            counter = 0;
+            skipped = 0;
+            //Sign check if true sign is plus , false is minus
+            bool sign = true;
+            //Return if time to adjust isnt proper
+            bool goB = false;
+            //Adjust time
+            int miliseconds;
+            string milis = textBox3.Text;
+            //output
+            string outputLine = "";
+
+            //Check sign
+            if (milis.Length > 1)
+            {
+                if (milis[0] == '-')
+                {
+                    milis = milis.Substring(1, milis.Length - 1);
+                    sign = false;
+                }
+            }
+
+            //Parsing to int string from textBox3
+            goB = int.TryParse(milis, out miliseconds);
+
+            //If something is wrong drop Message box and return from method
+            if (!goB || milis.Length == 0)
+            {
+                MessageBox.Show("Wpisz poprawny czas", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            //Looping through every line in file
+            foreach (string line in lines.ToList())
+            {
+                outputLine = line;
+
+                //Values to adjust (hours, minutes, seconds, miliseconds)
+                int h1;
+                int h2;
+                int m1;
+                int m2;
+                int s1;
+                int s2;
+                int ms1;
+                int ms2;
+
+                //Strings to paste in adjusted line
+                string Sh1;
+                string Sh2;
+                string Sm1;
+                string Sm2;
+                string Ss1;
+                string Ss2;
+                string Sms1;
+                string Sms2;
+
+                //For calculations
+                int calc;
+
+                //If line contain time stamps, looks like: 00:46:49,256 --> 00:46:50,915
+                if (line.Contains(@"-->") && line.Length == 29)
+                {
+                    //Counter for time stamps
+                    counter++;
+
+                    //Parsing lines to int
+                    int.TryParse(line.Substring(0, 2), out h1);
+                    int.TryParse(line.Substring(3, 2), out m1);
+                    int.TryParse(line.Substring(6, 2), out s1);
+                    int.TryParse(line.Substring(9, 3), out ms1);
+                    int.TryParse(line.Substring(17, 2), out h2);
+                    int.TryParse(line.Substring(20, 2), out m2);
+                    int.TryParse(line.Substring(23, 2), out s2);
+                    int.TryParse(line.Substring(26, 3), out ms2);
+
+                    //Calculations
+                    if (sign)
+                    {
+                        ms1 += miliseconds;
+
+                        if (ms1 >= 1000)
+                        {
+                            calc = ms1 / 1000;
+                            ms1 = ms1 % 1000;
+                            s1 += calc;
+                        }
+                        if (s1 >= 60)
+                        {
+                            calc = s1 / 60;
+                            s1 = s1 % 60;
+                            m1 += calc;
+                        }
+                        if (m1 >= 60)
+                        {
+                            calc = m1 / 60;
+                            m1 = m1 % 60;
+                            h1 += calc;
+                        }
+
+
+                        ms2 += miliseconds;
+
+                        if (ms2 >= 1000)
+                        {
+                            calc = ms2 / 1000;
+                            ms2 = ms2 % 1000;
+                            s2 += calc;
+                        }
+                        if (s2 >= 60)
+                        {
+                            calc = s2 / 60;
+                            s2 = s2 % 60;
+                            m2 += calc;
+                        }
+                        if (m2 >= 60)
+                        {
+                            calc = m2 / 60;
+                            m2 = m2 % 60;
+                            h2 += calc;
+                        }
+                    }
+
+                    else
+                    {
+                        ms1 -= miliseconds;
+
+                        if (ms1 < 0)
+                        {
+                            calc = -ms1 / 1000;
+                            ms1 = 1000 + (ms1 % 1000);
+                            s1 -= 1 + calc;
+                        }
+                        if (ms1 == 1000)
+                        {
+                            ms1 = 0;
+                            s1++;
+                        }
+                        if (s1 < 0)
+                        {
+                            calc = -s1 / 60;
+                            s1 = 60 + (s1 % 60);
+                            m1 -= 1 + calc;
+                        }
+                        if (s1 == 60)
+                        {
+                            s1 = 0;
+                            m1++;
+                        }
+                        if (m1 < 0)
+                        {
+                            calc = -m1 / 60;
+                            m1 = 60 + (m1 % 60);
+                            h1 -= 1 + calc;
+                        }
+                        if (m1 == 60)
+                        {
+                            m1 = 0;
+                            h1++;
+                        }
+
+                        ms2 -= miliseconds;
+
+                        if (ms2 < 0)
+                        {
+                            calc = -ms2 / 1000;
+                            ms2 = 1000 + (ms2 % 1000);
+                            s2 -= 1 + calc;
+                        }
+                        if (ms2 == 1000)
+                        {
+                            ms2 = 0;
+                            s2++;
+                        }
+                        if (s2 < 0)
+                        {
+                            calc = -s2 / 60;
+                            s2 = 60 + (s2 % 60);
+                            m2 -= 1 + calc;
+                        }
+                        if (s2 == 60)
+                        {
+                            s2 = 0;
+                            m2++;
+                        }
+                        if (m2 < 0)
+                        {
+                            calc = -m2 / 60;
+                            m2 = 60 + (m2 % 60);
+                            h2 -= 1 + calc;
+                        }
+                        if (m2 == 60)
+                        {
+                            m2 = 0;
+                            h2++;
+                        }
+                    }
+
+                    //If any of time stamps have negative time skip iteration
+                    if (h1 < 0 || h2 < 0 || m1 < 0 || m2 < 0 || s1 < 0 || s2 < 0 || ms1 < 0 || ms2 < 0)
+                    {
+                        skipped++;
+                        newLines.Add(line);
+                        continue;
+                    }
+
+                    //Adding zeros for proper .srt format
+                    Sh1 = AddZero(1, h1);
+                    Sh2 = AddZero(1, h2);
+                    Sm1 = AddZero(1, m1);
+                    Sm2 = AddZero(1, m2);
+                    Ss1 = AddZero(1, s1);
+                    Ss2 = AddZero(1, s2);
+                    Sms1 = AddZero(2, ms1);
+                    Sms2 = AddZero(2, ms2);
+
+                    //Adjusted line
+                    outputLine = $"{Sh1}:{Sm1}:{Ss1},{Sms1} --> {Sh2}:{Sm2}:{Ss2},{Sms2}";
+                }
+                //Adding line to list
+                newLines.Add(outputLine);
+            }
+            //List reset
+            lines = new List<string>();
+            lines = newLines;
+            UpdateTextBox(lines);
+        }
+
+        //Function for proper format
+        private string AddZero(int zeros, int number)
+        {
+            string output = "";
+
+            //For hours, minutes, seconds
+            if (zeros == 1)
+            {
+                if (number < 10)
+                {
+                    output = $"0{number}";
+                }
+                else
+                {
+                    output = $"{number}";
+                }
+            }
+            //For miliseconds
+            else if (zeros == 2)
+            {
+                if (number < 100)
+                {
+                    output = $"00{number}";
+                }
+                else if (number < 10)
+                {
+                    output = $"0{number}";
+                }
+                else
+                {
+                    output = $"{number}";
+                }
+            }
+
+            return output;
+        }
+
+
+        //Updating big text box with loaded file
+        private void UpdateTextBox(List<string> lineList)
+        {
+            string text = "";
+            foreach (string line in lineList)
+            {
+                text += line + "\r\n";
+            }
+            textBox1.Text = text;
+        }
+
+        //Extracting parent file path from file path of loaded file
+        private void GetParentPath(string filePath)
+        {
+            string[] parts = filePath.Split('\\');
+
+            int p = parts.Length;
+
+            parentPath = "";
+
+            for (int i = 0; i < p - 1; i++)
+            {
+                if (i == p - 2)
+                {
+                    parentPath += parts[i];
+                }
+                else
+                {
+                    parentPath += parts[i] + "\\";
+                }
+            }
+            fileName = parts[parts.Length - 1];
+        }
+
+        //Saving subtitles to new file in the same directions
+        private void SaveToFile()
+        {
+            string path = parentPath + "\\" + textBox4.Text + ".srt";
+            string messge = $"{counter - skipped} rekordów zostało zaktualizowanych" + "\r\n" + $"{skipped} rekordów nie udało się zaktualizować";
+            File.AppendAllText(path, textBox1.Text);
+            MessageBox.Show(messge, "Yay", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
